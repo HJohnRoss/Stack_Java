212c925656eeb687c75979a33e458e3e62c8ade8
FJohnHRoss <rossjohn936@gmail.com>
2022-11-25 12:39:22 -0800

    first commit

---
+++ week1/day1/first_java_program/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("My name is John Ross");
+        System.out.println("I am 20 years old");
+        System.out.println("My hometown is Las Vegas, NV");
+    }
+}

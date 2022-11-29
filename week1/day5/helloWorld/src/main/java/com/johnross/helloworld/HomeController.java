53303ea35f2189ebe3c6c10ce95587237f806fad
FJohnHRoss <rossjohn936@gmail.com>
2022-11-28 19:17:21 -0800

    daikichi

---
+++ week1/day5/helloWorld/src/main/java/com/johnross/helloworld/HomeController.java
@@ -0,0 +1,23 @@
+package com.johnross.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("greeting")
+public class HomeController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "Hello User";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello World";
+	}
+	@RequestMapping("/goodbye")
+	public String goodbye() {
+		return "goodbye";
+	}
+}
